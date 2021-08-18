b81673c9529d103cb4ac0b3784e3815c846661af
Jonathan Bukowsky <jonathan.bukowsky@revature.net>
2021-08-18 12:59:13 -0400

    Added Subtract function

--- week_1/Calculator/Program.cs
+++ week_1/Calculator/Program.cs
@@ -7,15 +7,26 @@ namespace Calculator
         static void Main(string[] args)
         {
             Add();
+            Subtract();
         }
 
-        static void Add()
+        static void Add(int input1, int input2)
         {
             // input stuff
             int input1 = int.Parse(Console.ReadLine()); // type inference, parsing
             int input2 = int.Parse(Console.ReadLine());
             // compute stuff
             int compute = (int)input1 + (int)input2; // type inference, casting
+            // output stuff
+            Console.WriteLine(compute);
+        }
+        static void Subtract(int input1, int input2)
+        {
+            // input stuff
+            int input1 = int.Parse(Console.ReadLine()); // type inference, parsing
+            int input2 = int.Parse(Console.ReadLine());
+            // compute stuff
+            int compute = (int)input1 - (int)input2; // type inference, casting
             // output stuff
             Console.WriteLine(compute);
         }
