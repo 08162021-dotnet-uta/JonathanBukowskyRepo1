fdfd00d7b73bef5a52fed6fec1cd334d94325707
Jonathan Bukowsky <jonathan.bukowsky@revature.net>
2021-08-18 12:52:36 -0400

    Create calculator project -- casting demo

---
+++ week_1/Calculator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // input stuff
+            int input1 = int.Parse(Console.ReadLine()); // type inference, parsing
+            int input2 = int.Parse(Console.ReadLine());
+            // compute stuff
+            int compute = (int)input1 + (int)input2; // type inference, casting
+            // output stuff
+            Console.WriteLine(compute);
+        }
+
+        static void Add(var a, var b)
+        {
+
+        }
+    }
+}
