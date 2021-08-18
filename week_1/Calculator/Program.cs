716c9ebe2a317653da9c7255964be4aeb3d6e086
Jonathan Bukowsky <jonathan.bukowsky@revature.net>
2021-08-18 12:59:34 -0400

    Refactored Add and Subtract

--- week_1/Calculator/Program.cs
+++ week_1/Calculator/Program.cs
@@ -6,29 +6,25 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Add();
-            Subtract();
+            // input stuff
+            int input1 = int.Parse(Console.ReadLine()); // type inference, parsing
+            int input2 = int.Parse(Console.ReadLine());
+            Add(input1, input2);
+            Subtract(input1, input2);
+
+            // output stuff
+            Console.WriteLine(compute);
         }
 
         static void Add(int input1, int input2)
         {
-            // input stuff
-            int input1 = int.Parse(Console.ReadLine()); // type inference, parsing
-            int input2 = int.Parse(Console.ReadLine());
             // compute stuff
             int compute = (int)input1 + (int)input2; // type inference, casting
-            // output stuff
-            Console.WriteLine(compute);
         }
         static void Subtract(int input1, int input2)
         {
-            // input stuff
-            int input1 = int.Parse(Console.ReadLine()); // type inference, parsing
-            int input2 = int.Parse(Console.ReadLine());
             // compute stuff
             int compute = (int)input1 - (int)input2; // type inference, casting
-            // output stuff
-            Console.WriteLine(compute);
         }
     }
 }
