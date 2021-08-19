7f4394a69bd877d1d7f3681c5251ad4c82e0cd36
Jonathan Bukowsky <jonathan.bukowsky@revature.net>
2021-08-19 16:09:55 -0400

    Add Project0

---
+++ projects/project_0/Project0.StoreApplication.Client/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Project0.StoreApplication.Domain.Models;
+
+namespace Project0.StoreApplication.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintAllStoreLocations();
+        }
+
+        static void PrintAllStoreLocations()
+        {
+            var storeLocations = new List<Store>()
+            {
+                new Store(),
+                new Store()
+            };
+
+            foreach (var store in storeLocations)
+            {
+                Console.WriteLine(store);
+            }
+        }
+    }
+}
