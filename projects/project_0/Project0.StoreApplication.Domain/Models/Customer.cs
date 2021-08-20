3f79c578b942523369b1c0de9ff86bb872357424
Jonathan Bukowsky <jonathan.bukowsky@revature.net>
2021-08-20 13:16:08 -0400

    Add Storage project

--- projects/project_0/Project0.StoreApplication.Domain/Models/Customer.cs
+++ projects/project_0/Project0.StoreApplication.Domain/Models/Customer.cs
@@ -1,7 +1,14 @@
+using System.Collections.Generic;
+
 namespace Project0.StoreApplication.Domain.Models
 {
-    class Customer
+    public class Customer
     {
-
+        public Customer()
+        {
+            // TODO: load this from somewhere
+            Purchases = new();
+        }
+        public List<Order> Purchases { get; }
     }
 }
---
+++ projects/project_0/Project0.StoreApplication.Storage/Repositories/StoreRepository.cs
@@ -0,0 +1,19 @@
+
+using System.Collections.Generic;
+using Project0.StoreApplication.Domain.Models;
+
+namespace Project0.StoreApplication.Storage.Repositories
+{
+    public class StoreRepository
+    {
+        public StoreRepository()
+        {
+            Stores = new List<Store> {
+                new Store() { Name = "Store001" },
+                new Store() { Name = "Store002" },
+                new Store() { Name = "Store003" }
+            };
+        }
+        public List<Store> Stores { get; set; }
+    }
+}
