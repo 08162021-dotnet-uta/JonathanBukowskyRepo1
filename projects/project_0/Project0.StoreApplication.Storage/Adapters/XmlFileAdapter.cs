135add5ed0f239ff6840dc8fb884124ee9de5087
Jonathan Bukowsky <jonathan.bukowsky@revature.net>
2021-08-23 16:20:22 -0400

    Clean up old code in XmlFileAdapter

--- projects/project_0/Project0.StoreApplication.Storage/Adapters/XmlFileAdapter.cs
+++ projects/project_0/Project0.StoreApplication.Storage/Adapters/XmlFileAdapter.cs
@@ -7,80 +7,12 @@ using Project0.StoreApplication.Domain.Models;
 
 namespace Project0.StoreApplication.Storage.Adapters
 {
-    internal class FileLocation
-    {
-        private const string dir = "/home/jon/revature/my_code/data/project_0/";
-        public const string Stores = dir + "stores.xml";
-        public const string Customers = dir + "customers.xml";
-        public const string Orders = dir + "orders.xml";
-        public const string Products = dir + "products.xml";
-
-        public static string GetPath(System.Type type)
-        {
-            string path;
-            if (type == typeof(Product))
-            {
-                path = Products;
-            }
-            else if (type == typeof(Customer))
-            {
-                path = Customers;
-            }
-            else if (type == typeof(Store))
-            {
-                path = Stores;
-            }
-            else if (type == typeof(Order))
-            {
-                path = Orders;
-            }
-            else
-            {
-                // TODO: handle this better
-                throw new System.Exception("Invalid object type in GetPath");
-            }
-            return path;
-        }
-
-    }
-
     public class XmlFileAdapter : Adapter
     {
-        /*
-        private XmlSerializer _storeSerial;
-        private XmlSerializer _productSerial;
-        private XmlSerializer _customerSerial;
-        private XmlSerializer _orderSerial;
-        */
-        public XmlFileAdapter()
-        {
-            //_storeSerial = new XmlSerializer(typeof(List<Store>));
-            //_productSerial = new XmlSerializer(typeof(List<Product>));
-            //_customerSerial = new XmlSerializer(typeof(List<Customer>));
-            //_orderSerial = new XmlSerializer(typeof(List<Order>));
-        }
         public override List<T> LoadAll<T>(string path)
         {
             return ReadListFromFile(new XmlSerializer(typeof(List<T>)), path) as List<T>;
         }
-        /*
-        public override List<Product> LoadProducts()
-        {
-            return ReadListFromFile(_productSerial, FileLocation.Products) as List<Product>;
-        }
-        public override List<Store> LoadStores()
-        {
-            return ReadListFromFile(_storeSerial, FileLocation.Stores) as List<Store>;
-        }
-        public override List<Customer> LoadCustomers()
-        {
-            return null;
-        }
-        public override List<Order> LoadOrders()
-        {
-            return null;
-        }
-        */
         private object ReadListFromFile(XmlSerializer xml, string path)
         {
             // file path
