8b21f3f91573d843bc9800ab3d8bf3a55fdf8fe7
Jon Bukowsky <jonrbuk@umich.edu>
2021-09-10 17:27:16 -0400

    Switch to webapi instead of webapp

--- projects/project_1/Project1.StoreApplication/Project1.StoreApplication.WebApi/Program.cs
+++ projects/project_1/Project1.StoreApplication/Project1.StoreApplication.WebApi/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace Project1.StoreApplication.WebClient
+namespace Project1.StoreApplication.WebApi
 {
     public class Program
     {
