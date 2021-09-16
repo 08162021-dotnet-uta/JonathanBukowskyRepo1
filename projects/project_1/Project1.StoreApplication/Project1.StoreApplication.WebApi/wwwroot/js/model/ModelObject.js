fcf6d6549d3e237c64713581419d3217396d6e32
Jon Bukowsky <jonrbuk@umich.edu>
2021-09-16 14:04:03 -0400

    Fix templating for ModelObjects (esp lists of modelObjects)

--- projects/project_1/Project1.StoreApplication/Project1.StoreApplication.WebApi/wwwroot/js/model/ModelObject.js
+++ projects/project_1/Project1.StoreApplication/Project1.StoreApplication.WebApi/wwwroot/js/model/ModelObject.js
@@ -1,6 +1,16 @@
 ﻿
-if (ModelObject === undefined) {
-    var ModelObject = (function () {
+if (Models === undefined) {
+    var Models = (function () {
+
+        // idea taken from stack overflow
+        // https://stackoverflow.com/questions/18884249/checking-whether-something-is-iterable
+        function isIterable(obj) {
+            if (obj == null) {
+                return false;
+            }
+            return typeof obj[Symbol.iterator] === 'function';
+        }
+
         class ModelObject {
             _identifiers = [];
 
@@ -18,31 +28,66 @@ if (ModelObject === undefined) {
                 });
             }
 
-            CreateElementFromTemplate(template) {
+            // populate in-place
+    //let elms = this.PopulateElement(elm, this[prop], prop, className);
+            PopulateElement(elm, data, propName, className, debug=false) {
+                if (debug) console.log("PopuElm", elm);
+                if (debug) console.log("data", data);
+                if (debug) console.log("propName", propName);
+                if (debug) console.log("className", className);
+                if (isIterable(data) && typeof data !== 'string') {
+                    if (debug) console.log("found a list of stuff");
+                    for (let obj of data) {
+                        // first, copy elm and add to container,
+                        // then replace it using PopulateElement
+                        let newElm = elm.cloneNode(true);
+                        elm.parentElement.insertBefore(newElm, elm);
+                        this.PopulateElement(newElm, obj, propName, className, debug);
+                    }
+                    elm.parentElement.removeChild(elm);
+                } else if (data instanceof ModelObject) {
+                    if (debug) console.log("Data? ", data);
+                    let populated = data.CreateElementFromTemplate(elm, className);
+                    elm.parentElement.replaceChild(populated, elm);
+                } else if (typeof (data) == 'object') {
+                    if (debug) console.log('object prop found', propName, data);
+                } else {
+                    if (debug) console.log("setting template data", elm, data);
+                    elm.textContent = data;
+                    elm.classList.remove(className);
+                }
+            }
+
+            CreateElementFromTemplate(template, templatePrefix='template', debug=false) {
+                if (debug) console.log("CreateElTemplate", template);
                 let newElm = template.cloneNode(true);
                 let templateClass = '';
                 newElm.classList.forEach(className => {
-                    if (className.includes('template')) {
+                    if (className.includes(templatePrefix)) {
                         templateClass = className;
                     }
                 });
                 if (!templateClass) {
+                    templateClass = templatePrefix;
+                }
+                if (!templateClass) {
                     throw new Error("Couldn't find template class");
                 }
+                if (debug) console.log("templateClass", templateClass);
                 this._identifiers.forEach(prop => {
                     let className = `${templateClass}-${prop}`;
                     let elms = Array.from(newElm.getElementsByClassName(className));
                     if (!elms.length) {
-                        //console.log(`Elm for ${className} not found`);
+                        if (debug) console.log(`Elm for ${className} not found`);
                     }
                     elms.forEach(elm => {
-                        //console.log(`Found Elm for ${className}`, elm);
-                        elm.textContent = this[prop];
-                        elm.classList.remove(className);
+                        if (debug) console.log(`Found Elm for ${className}`, elm);
+                        this.PopulateElement(elm, this[prop], prop, className, debug);
                     });
                 });
                 newElm.classList.remove(templateClass);
-                let generatedClass = templateClass.replace("template", "generated");
+                let generatedClass = templateClass.replace('template', "generated");
+                if (debug) console.log("Adding class to generated element", generatedClass);
                 newElm.classList.add('generated');
                 newElm.classList.add(generatedClass);
                 return newElm;
@@ -61,9 +106,77 @@ if (ModelObject === undefined) {
             });
         }
 
+        // ************************ PRODUCT ************************************
+        class Product extends ModelObject {
+            _identifiers = ['productId', 'name', 'price', 'description', 'categoryID', 'quantity', 'subtotal'];
+
+            constructor(product) {
+                super();
+                this.PopulateFromData(product);
+            }
+
+            get subtotal() {
+                let quantity = (this.quantity) ? this.quantity : 0;
+                let subtotal = this.price * quantity;
+                return subtotal;
+            }
+
+            set subtotal(value) {
+                return;
+            }
+        }
+
+        // ************************ ORDER ************************************
+        class Order extends ModelObject {
+            _identifiers = ['customer', 'orderID', 'products', 'store', 'total'];
+            constructor(order) {
+                super();
+                this.PopulateFromData(order);
+                this.products = this.products.map(p => new Product(p));
+                this.customer = new Customer(this.customer);
+                this.store = new Store(this.store);
+            }
+
+            get total() {
+                let total = 0;
+                this.products.forEach(prod => {
+                    total += prod.subtotal;
+                });
+                return total;
+            }
+
+            set total(value) {
+                return;
+            }
+        }
+
+        // ************************ ORDER ************************************
+        class Store extends ModelObject {
+            _identifiers = ['name', 'storeId', 'orders'];
+
+            constructor(store) {
+                super();
+                this.PopulateFromData(store);
+            }
+        }
+
+        // ************************ ORDER ************************************
+        class Customer extends ModelObject {
+            _identifiers = ['customerId', 'firstName', 'lastName', 'orders'];
+
+            constructor(customer) {
+                super();
+                this.PopulateFromData(customer);
+            }
+        }
+
+
         // "export" list
         return {
-            ModelObject,
+            Product,
+            Order,
+            Customer,
+            Store,
             RemoveGeneratedElements
         }
     })();
---
+++ projects/project_1/Project1.StoreApplication/Project1.StoreApplication.WebApi/wwwroot/js/orders.js
@@ -0,0 +1,56 @@
+﻿
+if (!Models) {
+    throw new Error("Must include ModelObjects");
+}
+
+function LoadOrders(type, container, template) {
+    function populateContainer(elms) {
+        if (!elms || !elms.length) {
+            throw new Error("No order data");
+        }
+        Models.RemoveGeneratedElements(container, 'generated-order');
+        elms.forEach(ord => {
+            let order = new Models.Order(ord);
+            let elm = order.CreateElementFromTemplate(template, 'template-order', true);
+            container.appendChild(elm);
+        });
+    }
+    if (type == "Store") {
+        let store = JSON.parse(sessionStorage.selectedStore);
+        if (!store) {
+            throw new Error("Must have store selected");
+        }
+        fetch(`/api/stores/${store.storeId}/orders`)
+            .then(res => {
+                return res.json();
+            }).then(data => {
+                populateContainer(data);
+            });
+    } else if (type == "Customer") {
+        let cust = JSON.parse(sessionStorage.user);
+        if (!cust) {
+            throw new Error("Must be logged in");
+        }
+        fetch(`/api/customers/${cust.customerId}/orders`)
+            .then(res => {
+                return res.json();
+            }).then(data => {
+                populateContainer(data);
+            })
+    } else {
+        throw new Error("Invalid type in LoadOrders");
+    }
+}
+
+window.addEventListener('load', function () {
+    let selector = document.querySelector('.history-selector');
+    console.log("adding handler...");
+
+    let orderContainer = document.querySelector(".order-list");
+    let templateOrder = document.querySelector(".template-order");
+    selector.addEventListener('change', function (e) {
+        if (e.target.value) {
+            LoadOrders(e.target.value, orderContainer, templateOrder);
+        }
+    });
+});
