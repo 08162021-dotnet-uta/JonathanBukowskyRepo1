6f953252b6a23a57057ad48a58ae460bc46c1db7
Jon Bukowsky <jonrbuk@umich.edu>
2021-09-16 17:55:13 -0400

    Add customers javascript to populate customer data on customers page

---
+++ projects/project_1/Project1.StoreApplication/Project1.StoreApplication.WebApi/wwwroot/js/customers.js
@@ -0,0 +1,22 @@
+﻿
+
+function LoadCustomers() {
+    let container = document.querySelector('.customer-list');
+    let template = document.querySelector('.template-customer');
+    fetch('/api/customers')
+        .then(res => {
+            return res.json();
+        }).then(data => {
+            Models.RemoveGeneratedElements(container, 'generated-customer');
+            data.forEach(cust => {
+                console.log(cust);
+                let customer = new Models.Customer(cust);
+                let custElm = customer.CreateElementFromTemplate(template);
+                container.appendChild(custElm);
+            });
+        });
+}
+
+window.addEventListener('load', function () {
+    LoadCustomers();
+});
