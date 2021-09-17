3a8d867f0a6793b15fbdcc6d48261e33c1c7bd7c
Jon Bukowsky <jonrbuk@umich.edu>
2021-09-16 20:28:35 -0400

    Add login and selectedStore guards

---
+++ projects/project_1/Project1.StoreApplication/Project1.StoreApplication.WebApi/wwwroot/js/guards/LoginGuard.js
@@ -0,0 +1,26 @@
+﻿
+function RedirectToLogin() {
+    window.location.href = "/index.html";
+}
+
+function ValidateLogin() {
+    let userJSON = sessionStorage.user;
+    if (!userJSON) {
+        RedirectToLogin();
+    }
+    let user;
+    try {
+        user = JSON.parse()
+    } catch {
+        RedirectToLogin();
+    }
+    if (!user) {
+        RedirectToLogin();
+    }
+    if (!user.customerId) {
+        RedirectToLogin();
+    }
+}
+
+// could wait for page load, but that's not necessary
+ValidateLogin();
---
+++ projects/project_1/Project1.StoreApplication/Project1.StoreApplication.WebApi/wwwroot/js/guards/SelectStoreGuard.js
@@ -0,0 +1,24 @@
+﻿
+
+function RedirectToStoreSelect() {
+    window.location.href = "/html/stores.html";
+}
+
+function ValidateSelectedStore() {
+    let storeJSON = sessionStorage.selectedStore;
+    if (!storeJSON) {
+        RedirectToStoreSelect();
+    }
+    let store;
+    try {
+        store = JSON.parse(storeJSON);
+    } catch {
+        RedirectToStoreSelect();
+    }
+    if (!store) {
+        RedirectToStoreSelect();
+    }
+    if (!store.storeId) {
+        RedirectToStoreSelect();
+    }
+}
